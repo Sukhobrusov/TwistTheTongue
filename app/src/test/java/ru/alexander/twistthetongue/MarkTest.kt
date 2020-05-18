b77be54780c271c5385f98bebfa6a0ced95cdb9b
Sasha Sukhobrusov <al.sukhobrusov@gmail.com>
2020-05-18 15:52:14 +0300

    #Comparing algorithm
    added comparing algorithm for finding differences between two strings of words when source string is longer than recognized

--- app/src/test/java/ru/alexander/twistthetongue/MarkTest.kt
+++ app/src/test/java/ru/alexander/twistthetongue/MarkTest.kt
@@ -7,15 +7,20 @@ import org.junit.Test
 class MarkTest {
     @Test
     fun testMark() {
-        println(compareStrings("Peter Piper picked a peck of pickled peppers", "peter piper picked a peck of pickled peppers"))
+        println(
+            compareStrings(
+                "Peter Piper picked a peck of pickled peppers",
+                "picked a peck of pickled"
+            )
+        )
     }
 
     private fun compareStrings(source: String, recognizedSpeech: String): Int {
-        val sourceWords = source.toLowerCase().replace('\n',' ').split(" ")
+        val sourceWords = source.toLowerCase().replace('\n', ' ').split(" ")
         val recognizedSpeechWords = recognizedSpeech.toLowerCase().split(" ")
 
         return if (sourceWords.size > recognizedSpeechWords.size) {
-            5
+            compareArraysPartially(sourceWords, recognizedSpeechWords)
         } else {
             compareArrays(sourceWords, recognizedSpeechWords)
         }
@@ -23,7 +28,10 @@ class MarkTest {
     }
 
 
-    private fun compareArrays(sourceWords : List<String>, recognizedSpeechWords : List<String>) : Int{
+    private fun compareArrays(
+        sourceWords: List<String>,
+        recognizedSpeechWords: List<String>
+    ): Int {
 
         val dif = recognizedSpeechWords.size - sourceWords.size
         val difArray = Array(sourceWords.size) { -1 }
@@ -53,6 +61,33 @@ class MarkTest {
                     i++
                 }
             }
+        }
+        return difArray.count { it == -1 }
+    }
+
+    fun compareArraysPartially(
+        sourceWords: List<String>,
+        recognizedSpeechWords: List<String>
+    ): Int {
+
+        val difArray = Array(sourceWords.size) { -1 }
+        val tolerance = 2
+
+        var i = 0
+        var j: Int
+        var lastSetIndex = -1
+
+        while (i < recognizedSpeechWords.size) {
+            j = lastSetIndex + 1
+            while (j < lastSetIndex + tolerance + 1 && j < sourceWords.size) {
+                if (sourceWords[j] == recognizedSpeechWords[i]) {
+                    lastSetIndex = j
+                    difArray[lastSetIndex] = 1
+                    break
+                }
+                j++
+            }
+            i++
         }
         return difArray.count { it == -1 }
     }
